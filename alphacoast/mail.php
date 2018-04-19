afce0fb1bb8da69de153679c7b0fb620b72755ef
Vlad <vladislavdudar1995@gmail.com>
2018-04-19 11:48:32 +0300

    19.04

--- alphacoast/mail.php
+++ alphacoast/mail.php
@@ -9,6 +9,9 @@ $name = trim($_POST["name"]);
 $mail = trim($_POST["mail"]);
 $subject = trim($_POST["subject"]);
 $textarea = trim($_POST["textarea"]);
+$phone2 = trim($_POST["phone-2"]);
+$email2 = trim($_POST["email"]);
+$textarea2 = trim($_POST["text-mess"]);
 
 $message = "
 <html>
@@ -22,7 +25,10 @@ $message = "
 	($name ? "Имя: " . $name . "<br/>" : '') .
 	($mail ? "E-mail: " . $mail . "<br/>" : '') .
 	($subject ? "Тема: " . $subject . "<br/>" : '') .
-	($textarea ? "Сообщение: " . $textarea . "<br/>" : '');
+	($textarea ? "Сообщение: " . $textarea . "<br/>" : '') .
+	($phone2 ? "Телефон: " . $phone2 . "<br/>" : '') .
+	($email2 ? "E-mail: " . $email2 . "<br/>" : '') .
+	($textarea2 ? "Сообщение: " . $textarea2 . "<br/>" : '');
 
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
